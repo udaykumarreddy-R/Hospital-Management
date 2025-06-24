fcdf8b73ca361f87d08dd2a0a4c646185ac73463
uday ram <udayram8425@gmail.com>
2025-06-24 17:48:47 +0530

    completed

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -96,18 +96,18 @@ WSGI_APPLICATION = "hospitalmanagement.wsgi.application"
 #     }
 # }
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hospitalmanagement',
-        'USER': 'root',
-        'PASSWORD': 'Uday@236',
-'HOST': 'your-internal-db-host.kuberns.local',
-
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
-
 
 
 # Password validation
