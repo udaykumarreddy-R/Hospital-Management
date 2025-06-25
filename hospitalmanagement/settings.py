775ea25de37f72af4a72e0b81fa610b7d5b75b3f
uday ram <udayram8425@gmail.com>
2025-06-25 09:15:23 +0530

    completed

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -32,7 +32,11 @@ TEMPLATE_DEBUG = DEBUG
 print("BASE_DIR: ", BASE_DIR)
 print("Templates DIRS: ", os.path.join(BASE_DIR, 'hospital', 'templates'))
 
-ALLOWED_HOSTS = ['.vercep.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    'hospital-management-159zb4e68-udaykumarreddy-rs-projects.vercel.app',
+    'localhost',  # useful for local development
+]
+
 
 # Application definition
 
